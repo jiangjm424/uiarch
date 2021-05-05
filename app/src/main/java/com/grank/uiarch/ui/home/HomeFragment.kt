19158066c75544cc2e7e18ad8245f8da78ebde0f
jiang.junmin <jiangjm8888@gmail.com>
2021-05-05 14:30:13 +0800

    init

---
+++ app/src/main/java/com/grank/uiarch/ui/home/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.grank.uiarch.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import com.grank.uiarch.R
+import com.grank.uiarch.ui.home.tab.SectionsPagerAdapter
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val sectionsPagerAdapter = SectionsPagerAdapter(requireContext(), childFragmentManager)
+        val viewPager: ViewPager = root.findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = root.findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+        val fab: FloatingActionButton = root.findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        return root
+    }
+}
---
+++ app/src/main/java/com/grank/uiarch/ui/home/tab/CommonTab.kt
@@ -0,0 +1,4 @@
+package com.grank.uiarch.ui.home.tab
+
+class CommonTab {
+}
