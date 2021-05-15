8d170053db7152e520eacb68c4d42c4c12d97a32
jiang.junmin <jiangjm8888@gmail.com>
2021-05-15 17:12:53 +0800

    添加外部路由中转，需要根据项目实际情况修改androidmainfest.xml中的配置

--- routercontroller/src/main/java/com/grank/routercontroller/DispatcherActivity.kt
+++ routercontroller/src/main/java/com/grank/routercontroller/DispatcherActivity.kt
@@ -1,6 +1,15 @@
 package com.grank.routercontroller
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.launcher.ARouter
 
 class DispatcherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        intent.data?.let {
+            ARouter.getInstance().build(it).navigation()
+        }
+        finish()
+    }
 }
